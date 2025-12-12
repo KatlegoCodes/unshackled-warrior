ecff912e1996a79d18fce43c3934184f7b4934cb
Frost <moosemashegona@gmail.com>
2025-12-12 16:40:45 +0200

    added the Hero component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Hero = () => {
   return (
-    <div className="bg-black h-screen">
+    <div className="bg-black h-15">
       <h1 className="text-white">Hero</h1>
     </div>
   );
