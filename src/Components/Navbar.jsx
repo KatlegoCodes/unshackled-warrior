a1d6065178697c7fcf7d69c16656761705fc4292
Frost <moosemashegona@gmail.com>
2025-12-13 13:47:50 +0200

    Added the pop up menu for small screens

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { Menu, X, ShoppingCart } from "lucide-react";
 
 export const Navbar = () => {
@@ -33,16 +34,63 @@ export const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-4">
-          <ShoppingCart className="text-white hover:text-lime-200 cursor-pointer" />
+          <ShoppingCart className="text-white hover:text-lime-200 cursor-pointer md:block hidden" />
 
           <button
-            className="text-white md:hidden"
+            className="text-white md:hidden cursor-pointer"
             onClick={() => setIsOpen(!isOpen)}
           >
             {isOpen ? <X size={28} /> : <Menu size={28} />}
           </button>
         </div>
       </div>
+
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <motion.div
+              className="fixed inset-0 md:hidden bg-black/40"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            />
+
+            <motion.div
+              className="fixed right-0 top-0 h-full w-64 bg-gray-800 p-6 flex flex-col gap-6 md:hidden z-50"
+              initial={{ x: 300 }}
+              animate={{ x: 0 }}
+              exit={{ x: 300 }}
+              transition={{ type: "spring", stiffness: 200, damping: 25 }}
+            >
+              <button
+                onClick={() => setIsOpen(false)}
+                className="text-white self-end mb-4 cursor-pointer"
+              >
+                <X size={28} />
+              </button>
+
+              <a href="#Home" className="block text-white hover:text-lime-300">
+                home
+              </a>
+              <a href="#Shop" className="block text-white hover:text-lime-300">
+                shop
+              </a>
+              <a
+                href="#Contact"
+                className="block text-white hover:text-lime-300"
+              >
+                contact
+              </a>
+              <a
+                href="#About"
+                className="block text-white hover:text-lime-300 mb-5"
+              >
+                about
+              </a>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
