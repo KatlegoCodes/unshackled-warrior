564517d1133ff591177e50b99d6ab893d80c53c4
Frost <moosemashegona@gmail.com>
2025-12-13 21:17:52 +0200

    Created the featuredCollection component and mounted it

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Navbar } from "./Components/Navbar";
 import { Hero } from "./Components/Hero";
 import { Footer } from "./Components/Footer";
+import { FeaturedCollection } from "./Components/FeaturedCollection";
 
 export const App = () => {
   return (
     <div className="w-full">
       <Navbar />
       <Hero />
+      <FeaturedCollection />
       <Footer />
     </div>
   );
