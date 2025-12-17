731e2c8d7557b58fe736753fb86fa495739ef0d7
Frost <moosemashegona@gmail.com>
2025-12-17 17:26:20 +0200

    Added more products to the assets file

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,39 +1,51 @@
-import { image } from "framer-motion/client"
 import project_img1 from "./Hoody-back.jpg";
 import project_img2 from "./Hoody-front.jpg";
 import project_img3 from "./back-tank.jpg";
 import project_img4 from "./shake-bottle.png";
+import project_img5 from "./sleeveless-hoodie.png";
+import project_img6 from "./Unshackled-warrior-cap.png";
 
-export const assets = {
-    project_img1,
-    project_img2,
-    project_img3,
-    project_img4
-}
+
 
 export const projectData = [
     {
+        id: 1,
         name: "Unshackle-Them Hoody",
         price: "R600",
         category: "Hoodie",
-        image: assets.project_img1,
+        image: project_img1,
     },
     {
+        id: 2,
         name: "Unshackled Warrior Hoody",
         price: "R600",
         category: "Hoodie",
-        image: assets.project_img2,
+        image: project_img2,
     },
-    {
+    {   id: 3,
         name: "Unshackled Warrior Tank",
         price: "R400",
         category: "Tank",
-        image: assets.project_img3,
+        image: project_img3,
     },
-    {
+    {   id: 4,
         name: "Protein Shake bottle",
         price: "R100",
         category: "accessories",
-        image: assets.project_img4,
+        image: project_img4,
+    },
+    {   id: 5,
+        name: "Sleeveless Hoodie",
+        price: "R650",
+        category: "Hoodie",
+        image: project_img5,
+    },
+    {   id: 6,
+        name: "Unshackled Warrior Cap",
+        price: "R250",
+        category: "accessories",
+        image: project_img6,
     }
 ]
+
+export default projectData;
