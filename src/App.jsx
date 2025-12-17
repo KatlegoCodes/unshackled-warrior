6aa024f755c6e7180b181fb96d33ba44658f9b87
Frost <moosemashegona@gmail.com>
2025-12-17 15:42:03 +0200

    defined routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./Components/Navbar";
-import { Hero } from "./Components/Hero";
+import { Home } from "./Pages/Home";
+import { About } from "./Pages/About";
+import { Shop } from "./Pages/Shop";
+import { Cart } from "./Pages/Cart";
 import { Footer } from "./Components/Footer";
-import { FeaturedCollection } from "./Components/FeaturedCollection";
-import { Cta } from "./Components/Cta";
 
 export const App = () => {
   return (
     <div className="w-full">
       <Navbar />
-      <Hero />
-      <FeaturedCollection />
-      <Cta />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+
       <Footer />
     </div>
   );
