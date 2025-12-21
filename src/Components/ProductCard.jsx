2b43768ea5e393cd312add07eae8cba09fd4fcba
Frost <moosemashegona@gmail.com>
2025-12-21 23:11:24 +0200

    Added the ProductCard component for reusability

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ShoppingCart } from "lucide-react";
+import { motion } from "framer-motion";
+
+export const ProductCard = ({ product, showCategory = true }) => {
+  return (
+    <motion.div
+      className="bg-gray-900 rounded-2xl p-4 shadow-lg hover:shadow-2xl transition group border border-amber-50/10"
+      whileHover={{ y: -6 }}
+      transition={{ type: "spring", stiffness: 200 }}
+    >
+      <div className="relative bg-gray-700 rounded-xl overflow-hidden mb-4">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-full h-64 object-cover hover:scale-105 transition duration-300"
+        />
+      </div>
+
+      <h3 className="font-semibold text-lg text-white m-2">{product.name}</h3>
+
+      {showCategory && (
+        <p className="text-sm text-gray-400 m-2">{product.category}</p>
+      )}
+
+      <p className="mt-2 text-lg font-bold text-white m-2">{product.price}</p>
+
+      <button className="flex items-center justify-center w-full mt-4 gap-2 bg-lime-400 text-black px-16 py-2 rounded-2xl font-semibold transition hover:scale-105">
+        <ShoppingCart size={18} />
+        Add to cart
+      </button>
+    </motion.div>
+  );
+};
