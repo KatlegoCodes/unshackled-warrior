bae6a08fd08b6dc5d680e6b0849c5d3a8eae3c48
Frost <moosemashegona@gmail.com>
2025-12-21 22:18:45 +0200

    Added the shop component

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { ProductCart } from "../Components/ProjectCard";
+import { projectData } from "../assets/assets";
 
 export const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <section className="bg-gray-900 min-h-screen text-white py-20 px-6">
+      <div>
+        <h1>Shop Collection</h1>
+      </div>
+    </section>
+  );
 };
